bb4c61ec9072c224da27ef9e7305a91f604b9d25
Aashishnehare <aashishnehare@outlook.com>
2023-10-08 08:03:23 +0530

    demo

--- demo.java
+++ demo.java
@@ -2,7 +2,7 @@ class Simple{
 	
     public static void main(String args[]){  
 	
-     System.out.println("Welcome to Jenkins");  
+     System.out.println("Welcome to Nehare Venture's");  
 	
     }  
 	
